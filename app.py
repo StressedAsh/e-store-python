b5b58a6ef2aa2d471d7d7f80b6865e4e76c8e86e
ThatStressedGuy <ashutosh.dhatwalia52@gmail.com>
2024-04-09 13:28:48 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+import csv
+from db import db
+from models import Customer, Products, Order, ProductOrder
+from pathlib import Path
+from routes import api_customers_bp , api_products_bp,api_order_bp,htmls_bp
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.instance_path = Path("./data").resolve()
+db.init_app(app)
+app.register_blueprint(api_customers_bp, url_prefix="/api/customers")
+
+app.register_blueprint(api_products_bp, url_prefix="/api/products")
+
+app.register_blueprint(api_order_bp, url_prefix="/api/orders")
+app.register_blueprint(htmls_bp, url_prefix="/")
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
---
+++ routes/api_customers.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, jsonify, request, redirect, url_for
+from db import db
+from models import Customer
+
+# Create a Blueprint for the customers API
+api_customers_bp = Blueprint("api_customers", __name__)
+
+@api_customers_bp.route("/", methods=["GET"])
+def get_customers():
+    statement = db.select(Customer).order_by(Customer.name)
+    records = db.session.execute(statement)
+    results = records.scalars()
+    return jsonify([cust.to_json() for cust in results])
+
+@api_customers_bp.route("/<int:id>", methods=["PUT"])
+def update_balance(id):
+    data = request.get_json()
+    customer = db.get_or_404(Customer, id)
+    if "balance" not in data:
+        return "Invalid Request", 405
+    balance = data["balance"]
+    if not isinstance(balance, (int, float)):
+        return "Invalid Request", 405
+    customer.balance = balance
+    db.session.commit()
+    return "", 204
+
+@api_customers_bp.route("/<int:id>", methods=["DELETE"])
+def delete_customer(id):
+    customer = db.get_or_404(Customer, id)
+    db.session.delete(customer)
+    db.session.commit()
+    return "", 204
+
+@api_customers_bp.route("/", methods=["POST"])
+def customers_json():
+    # ! This method uses the form in my html so not as per the project instructions
+    if request.form:
+        data = request.form.to_dict()
+        customer = Customer(**data)
+        db.session.add(customer)
+        db.session.commit()
+    else:
+        data = request.get_json()
+        if "name" not in data or "balance" not in data or "phone" not in data:
+            return "Invalid Request", 400
+        name = data["name"]
+        if not isinstance(name, str):
+            return "Invalid Request", 400
+        balance = data["balance"]
+        if not isinstance(balance, (int, float)) or balance < 0:
+            return "Invalid Request", 400
+        phone = data["phone"]
+        db.session.add(Customer(name=name, balance=balance, phone=phone))
+        db.session.commit()
+    return redirect(url_for("html.customers_info"))
